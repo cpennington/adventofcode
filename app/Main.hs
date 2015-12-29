b5e8b7218bcff207f08bd40cbef4ddf2928034d0
Calen Pennington <cale@edx.org>
2015-12-29 00:12:25 -0500

    Solve day 3

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Lib
 import System.Environment (getArgs)
 import Advent.Day1
 import Advent.Day2
+import Advent.Day3
 
 main :: IO ()
 main = do
@@ -16,6 +17,8 @@ main = do
         (1, "b") -> show $ Advent.Day1.solveB input
         (2, "a") -> show $ Advent.Day2.solveA input
         (2, "b") -> show $ Advent.Day2.solveB input
+        (3, "a") -> show $ Advent.Day3.solveA input
+        (3, "b") -> show $ Advent.Day3.solveB input
         _        -> day ++ puzzle ++ " isn't ready yet!"
 
   putStrLn result
---
+++ src/Advent/Day3.hs
@@ -0,0 +1,32 @@
+module Advent.Day3 where
+
+import Data.Set (fromList)
+
+data Dir = N | S | E | W
+type Loc = (Int, Int)
+
+parseAll :: String -> [Dir]
+parseAll = map parse
+
+parse :: Char -> Dir
+parse '^' = N
+parse '<' = W
+parse '>' = E
+parse 'v' = S
+
+move :: Loc -> Dir -> Loc
+move (x, y) N = (x, y+1)
+move (x, y) S = (x, y-1)
+move (x, y) E = (x+1, y)
+move (x, y) W = (x-1, y)
+
+
+split :: [a] -> ([a], [a])
+split = foldr split' ([], [])
+  where split' x (es, os) = (os, x:es)
+
+santa = scanl move (0, 0)
+robo dirs = santa es ++ santa os
+  where (es, os) = split dirs
+solveA = length . fromList . santa . parseAll
+solveB = length . fromList . robo . parseAll
