4db20f934bedae2d8664105818be82b0ef7fd05f
Calen Pennington <cale@edx.org>
2016-01-30 07:52:22 -0500

    Day 9, with part b, but lacking abstraction

--- src/Advent/Day9.hs
+++ src/Advent/Day9.hs
@@ -14,16 +14,25 @@ import Data.Function (on)
 
 import Debug.Trace
 
+traceResult a b = traceShow (a, b) b
+
 type TSMap = Map String (Map String Integer)
+type PathAccum a c = Path a c -> Maybe (Path a c) -> ChoiceTree (Step a c) -> Maybe (Path a c)
 type PathCmp a c = Path a c -> Path a c -> Bool
 
-data Step a c = Step a c
-    deriving Show
-data Path a c = Path [a] c
-    deriving Show
+data Step a c = Step
+    { sStep :: a
+    , sCost :: c
+    } deriving Show
+data Path a c = Path
+    { pSteps :: [a]
+    , pCost :: c
+    } deriving Show
 
-data ChoiceTree a = Choice a [ChoiceTree a]
-  deriving Show
+data ChoiceTree a = Choice
+    { cNode :: a
+    , cNext :: [ChoiceTree a]
+    } deriving Show
 
 parseAll :: String -> TSMap
 parseAll input = foldl' (flip addEntry) empty parsed
@@ -61,62 +70,39 @@ betterPath isBetter (Just l) (Just r)
     | l `isBetter` r = Just l
     | otherwise = Just r
 
-bestPath :: (Num c, Show a, Show c) => PathCmp a c -> Maybe (Path a c) -> Path a c -> [ChoiceTree (Step a c)] -> Maybe (Path a c)
-bestPath isBetter best current steps = best''
-    where best' = bestPathHead isBetter best current steps
-          best'' = bestPathTail isBetter best' current steps
+bestPath :: (Num c, Show a, Show c) => PathAccum a c -> Path a c -> Maybe (Path a c) -> [ChoiceTree (Step a c)] -> Maybe (Path a c)
+bestPath isBetter current best steps = foldl' (isBetter current) best steps
 
-bestPathHead :: (Num c, Show a, Show c) => PathCmp a c -> Maybe (Path a c) -> Path a c -> [ChoiceTree (Step a c)] -> Maybe (Path a c)
-bestPathHead isBetter best current [] = traceShow ("head, []", best, current, result) result
-    where result = betterPath isBetter best (Just current)
-bestPathHead isBetter best current s@((Choice step cs):_) = traceShow ("head, cs", best, current, s, result) result
-    where result = if (isJust best) && (fromJust best) `isBetter` next
-                     then best
-                     else bestPath isBetter best next cs
-          next = current .+. step
-
-bestPathTail :: (Num c, Show a, Show c) => PathCmp a c -> Maybe (Path a c) -> Path a c -> [ChoiceTree (Step a c)] -> Maybe (Path a c)
-bestPathTail isBetter best current [] = traceShow ("tail, []", best, current, result) result
-    where result = best
-bestPathTail isBetter best current s@[_] = traceShow ("tail, [_]", best, current, s, result) result
-    where result = best
-bestPathTail isBetter best current s@(_:cs) = traceShow ("tail, cs", best, current, s, result) result
-    where result = bestPath isBetter best current cs
-
--- shortest :: (Num c, Ord c) => Maybe (Path a c) -> c -> [ChoiceTree (Step a c)] -> Maybe (Path a c)
--- shortest minPath curCost [] = minPath
--- shortest minPath curCost ((Choice (Step step stepCost) []):[]) = betterPath minPath $ Just $ Path [step] (curCost + stepCost)
--- shortest minPath curCost ((Choice (Step step stepCost) next):[]) = shortest minPath (curCost + stepCost) next
--- shortest minPath curCost ((Choice (Step step stepCost) []):rest) = shortest (betterPath minPath takeStep) curCost rest
---     where takeStep = Just $ Path [step] (curCost + stepCost)
--- shortest Nothing curCost ((Choice (Step step stepCost) next):rest) = case shortest Nothing (curCost + stepCost) next of
---         Nothing -> Nothing
---         Just (Path path cost) ->
---             let takeStep = Just $ Path (step:path) cost
---             in case shortest takeStep curCost rest of
---             Nothing -> Just $ Path (step:path) cost
---             Just p -> Just p
-
--- shortest minPath@(Just (Path _ minCost)) curCost ((Choice (Step step stepCost) next):rest) = if curCost + stepCost > minCost
---     then fallbackPath
---     else case shortest minPath (curCost + stepCost) next of
---         Nothing -> fallbackPath
---         Just (Path path cost) ->
---             let takeStep = Just $ Path (step:path) cost
---             in case shortest takeStep curCost rest of
---             Nothing -> takeStep
---             Just p -> Just p
---     where fallbackPath = shortest minPath curCost rest
-
-tsp :: PathCmp String Integer -> TSMap -> Integer
-tsp isBetter m = case bestPath isBetter Nothing (Path [] 0) (traceShowId $ paths m) of
+tsp :: PathAccum String Integer -> TSMap -> Integer
+tsp isBetter m = case bestPath isBetter (Path [] 0) Nothing (paths m) of
     Nothing -> error "Unable to find a path"
     Just (Path p c) -> c
 
+isBetterA :: PathAccum String Integer
+isBetterA current Nothing (Choice node []) = traceResult (current, Nothing :: Maybe (Step String Integer), node) $ Just $ current .+. node
+isBetterA current Nothing choice = traceResult (current, Nothing :: Maybe (Step String Integer), choice) $ bestPath isBetterA (current .+. cNode choice) Nothing (cNext choice)
+isBetterA current (Just best) choice = traceResult (current, best, choice) $ if best .<. current'
+    then Just best
+    else case cNext choice of
+            [] -> betterPath (.<.) (Just best) (Just current')
+            next -> bestPath isBetterA current' (Just best) next
+    where
+        (.<.) = ((<) `on` pCost)
+        current' = current .+. cNode choice
+
+isBetterB :: PathAccum String Integer
+isBetterB current Nothing (Choice node []) = traceResult (current, Nothing :: Maybe (Step String Integer), node) $ Just $ current .+. node
+isBetterB current Nothing choice = traceResult (current, Nothing :: Maybe (Step String Integer), choice) $ bestPath isBetterB (current .+. cNode choice) Nothing (cNext choice)
+isBetterB current (Just best) choice = traceResult (current, best, choice) $ case cNext choice of
+            [] -> betterPath (.<.) (Just best) (Just current')
+            next -> bestPath isBetterB current' (Just best) next
+    where
+        (.<.) = ((>) `on` pCost)
+        current' = current .+. cNode choice
+
 instance Solution 9 "a" where
-    solve (Input input) = Output $ show $ tsp ((<) `on` cost) $ parseAll input
-        where cost (Path a c) = c
+    solve (Input input) = Output $ show $ tsp isBetterA $ parseAll input
 
 instance Solution 9 "b" where
-    solve (Input input) = Output $ show $ tsp ((>) `on` cost) $ parseAll input
+    solve (Input input) = Output $ show $ tsp isBetterB $ parseAll input
         where cost (Path a c) = c
