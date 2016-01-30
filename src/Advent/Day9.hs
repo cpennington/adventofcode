745ded4cf80acbce47066aeba71af5474464fe70
Calen Pennington <cale@edx.org>
2016-01-30 08:02:13 -0500

    Minimize set of differing values passed in Day9 between a and b

--- src/Advent/Day9.hs
+++ src/Advent/Day9.hs
@@ -78,31 +78,20 @@ tsp isBetter m = case bestPath isBetter (Path [] 0) Nothing (paths m) of
     Nothing -> error "Unable to find a path"
     Just (Path p c) -> c
 
-isBetterA :: PathAccum String Integer
-isBetterA current Nothing (Choice node []) = traceResult (current, Nothing :: Maybe (Step String Integer), node) $ Just $ current .+. node
-isBetterA current Nothing choice = traceResult (current, Nothing :: Maybe (Step String Integer), choice) $ bestPath isBetterA (current .+. cNode choice) Nothing (cNext choice)
-isBetterA current (Just best) choice = traceResult (current, best, choice) $ if best .<. current'
+isBetter :: (Show a, Show c, Num c) => PathCmp a c -> PathCmp a c -> PathAccum a c
+isBetter _ _ current Nothing (Choice node []) = traceResult (current, Nothing :: Maybe (Step String Integer), node) $ Just $ current .+. node
+isBetter better strictlyBetter current Nothing choice = traceResult (current, Nothing :: Maybe (Step String Integer), choice) $ bestPath (isBetter better strictlyBetter) (current .+. cNode choice) Nothing (cNext choice)
+isBetter better strictlyBetter current (Just best) choice = traceResult (current, best, choice) $ if best `strictlyBetter` current'
     then Just best
     else case cNext choice of
-            [] -> betterPath (.<.) (Just best) (Just current')
-            next -> bestPath isBetterA current' (Just best) next
+            [] -> betterPath better (Just best) (Just current')
+            next -> bestPath (isBetter better strictlyBetter) current' (Just best) next
     where
-        (.<.) = ((<) `on` pCost)
-        current' = current .+. cNode choice
-
-isBetterB :: PathAccum String Integer
-isBetterB current Nothing (Choice node []) = traceResult (current, Nothing :: Maybe (Step String Integer), node) $ Just $ current .+. node
-isBetterB current Nothing choice = traceResult (current, Nothing :: Maybe (Step String Integer), choice) $ bestPath isBetterB (current .+. cNode choice) Nothing (cNext choice)
-isBetterB current (Just best) choice = traceResult (current, best, choice) $ case cNext choice of
-            [] -> betterPath (.<.) (Just best) (Just current')
-            next -> bestPath isBetterB current' (Just best) next
-    where
-        (.<.) = ((>) `on` pCost)
         current' = current .+. cNode choice
 
 instance Solution 9 "a" where
-    solve (Input input) = Output $ show $ tsp isBetterA $ parseAll input
+    solve (Input input) = Output $ show $ tsp (isBetter (.<.) (.<.)) $ parseAll input
+        where (.<.) = (<) `on` pCost
 
 instance Solution 9 "b" where
-    solve (Input input) = Output $ show $ tsp isBetterB $ parseAll input
-        where cost (Path a c) = c
+    solve (Input input) = Output $ show $ tsp (isBetter ((>) `on` pCost) (const $ const False)) $ parseAll input
